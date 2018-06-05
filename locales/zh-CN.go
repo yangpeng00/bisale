5c7ad254828c869b2fbeb791694fe9fd3406dead
koyeo <im@koyeo.io>
2018-06-05 16:20:30 +0800

    添加请求过滤

--- locales/zh-CN.go
+++ locales/zh-CN.go
@@ -3,11 +3,14 @@ package locales
 import "bisale/bisale-console-api/codes"
 
 var zhCN = map[int32]string{
-	codes.Success:               "操作成功",
-	codes.SendCodeLock60Seconds: "请60秒后再试",
-	codes.ServiceError:          "服务调用错误",
-	codes.AccessTokenIsEmpty:    "AccessToken 不能为空",
-	codes.AccessTokenIsInvalid:  "请重新登录",
-	codes.FormIsEmpty:           "提交数据不能为空",
-	codes.ValidateError:         "数据校验错误",
+	codes.Success:                "操作成功",
+	codes.SendCodeLock60Seconds:  "请60秒后再试",
+	codes.ServiceError:           "服务调用错误",
+	codes.AccessTokenIsEmpty:     "AccessToken 不能为空",
+	codes.AccessTokenIsInvalid:   "请重新登录",
+	codes.RepeatRequestStrict:    "正在处理中",
+	codes.RepeatRequestWithIp:    "重复点击",
+	codes.RepeatRequestWithToken: "正在处理中",
+	codes.FormIsEmpty:            "提交数据不能为空",
+	codes.ValidateError:          "数据校验错误",
 }
