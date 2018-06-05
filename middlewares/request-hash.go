3faaf2871895c6cb5504fbe5520758887499c30e
koyeo <im@koyeo.io>
2018-06-05 16:27:36 +0800

    添加请求过滤

--- middlewares/request-hash.go
+++ middlewares/request-hash.go
@@ -4,26 +4,31 @@ import (
 	"github.com/labstack/echo"
 	"bytes"
 	"io/ioutil"
-	"encoding/json"
 	"crypto/md5"
 	"encoding/hex"
+	"bisale/bisale-console-api/common"
+	"github.com/sirupsen/logrus"
 )
 
 func RequestHash(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		var params interface{}
 		var data string
 		req := c.Request()
-		params = c.QueryParams()
+		url := c.Request().URL.String()
 		if req.ContentLength != 0 {
 			body, _ := ioutil.ReadAll(req.Body)
 			req.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 			data = string(body)
 		}
-		paramsJson, _ := json.Marshal(params)
+
 		hasher := md5.New()
-		hasher.Write([]byte(string(paramsJson) + data))
-		c.Request().Header.Set("X-Request-Hash", hex.EncodeToString(hasher.Sum(nil)))
+		hasher.Write([]byte(url + data))
+		hash := hex.EncodeToString(hasher.Sum(nil))
+		c.Request().Header.Set("X-Request-Hash", hash)
+		common.Log.WithFields(logrus.Fields{
+			"url":  url,
+			"hash": hash,
+		})
 		if err := next(c); err != nil {
 			c.Error(err)
 		}
