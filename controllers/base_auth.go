96b16951fefab9be60e8754849330b71834a749f
koyeo <im@koyeo.io>
2018-06-05 23:53:29 +0800

    修复登录

--- controllers/base_auth.go
+++ controllers/base_auth.go
@@ -8,12 +8,17 @@ import (
 	"bisale/bisale-console-api/config"
 	"bisale/bisale-console-api/thrift/message"
 	accountInputs "bisale/bisale-console-api/thrift/inputs"
+	"fmt"
 )
 
 type LoginForm struct {
 	Username string `json:"username" validate:"required"`
 	Code     string `json:"code" validate:"required"`
 	Key      string `json:"key"`
+}
+
+type MobileForm struct {
+	Mobile string `json:"mobile"`
 }
 
 func GetLoginCodeIdentify(mobile string) string {
@@ -23,19 +28,29 @@ func GetLoginCodeIdentify(mobile string) string {
 func PostLoginSMSCode(c echo.Context) error {
 	log, traceId := common.GetLoggerWithTraceId(c)
 
+	mobileForm := new(MobileForm)
+
+	if err := c.Bind(mobileForm); err != nil {
+		return Status(c, codes.FormIsEmpty, err)
+	}
+	if err := c.Validate(mobileForm); err != nil {
+		return Status(c, codes.ValidateError, err)
+	}
+
+
 	messageService, messageClient := common.GetMessageServiceClient()
 	defer common.MessageServicePool.Put(messageClient)
 	captchaService, captchaClient := common.GetCaptchaServiceClient()
 	defer common.CaptchaServicePool.Put(captchaClient)
 
 	ctx := context.Background()
-	mobile := c.FormValue("mobile")
-	identify := GetLoginCodeIdentify(mobile)
+
+	identify := GetLoginCodeIdentify(mobileForm.Mobile)
 	times, _ := captchaService.GetCount(ctx, traceId, identify)
 	if times > 10 {
 		return Status(c, codes.SendCodeLock60Seconds, "")
 	}
-	numberCode, err := captchaService.GenerateNumberCode(ctx, traceId, mobile, 6, 600)
+	numberCode, err := captchaService.GenerateNumberCode(ctx, traceId, mobileForm.Mobile, 6, 600)
 	if err != nil {
 		log.Error(err)
 		return err
@@ -43,7 +58,7 @@ func PostLoginSMSCode(c echo.Context) error {
 
 	appId := "bisale-console-api"
 	template := "template::sms::login-code"
-	err = messageService.SendSMS(ctx, traceId, appId, mobile, template, "{\"code\":"+numberCode.Value+"}", "zh-CN", 60)
+	err = messageService.SendSMS(ctx, traceId, appId, mobileForm.Mobile, template, "{\"code\":"+numberCode.Value+"}", "zh-CN", 60)
 
 	if err != nil {
 		log.Error(err)
