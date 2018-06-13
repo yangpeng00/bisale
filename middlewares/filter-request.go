0645885daf69085b6181aac9c6a144eeaf914f5f
koyeo <im@koyeo.io>
2018-06-13 15:06:19 +0800

    限制POST/PUT请求过滤

--- middlewares/filter-request.go
+++ middlewares/filter-request.go
@@ -22,24 +22,26 @@ func hashString(s ...string) string {
 
 func FilterRequestsWithIp(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		hash := c.Request().Header.Get("X-Request-Hash")
-		ip := c.RealIP()
-		cacheKey := hashString(config.Config.Redis.CachePrefix, "ip-filter", ip, hash)
-		_, err := common.Cache.Get(cacheKey).Result()
+		if c.Request().Method == echo.POST || c.Request().Method == echo.PUT {
+			hash := c.Request().Header.Get("X-Request-Hash")
+			ip := c.RealIP()
+			cacheKey := hashString(config.Config.Redis.CachePrefix, "ip-filter", ip, hash)
+			_, err := common.Cache.Get(cacheKey).Result()
 
-		if err != nil && err != redis.Nil {
-			return controllers.Status(c, codes.CacheError, err)
-		}
+			if err != nil && err != redis.Nil {
+				return controllers.Status(c, codes.CacheError, err)
+			}
 
-		if err != redis.Nil {
-			return controllers.Status(c, codes.RepeatRequestWithIp, err)
-		}
-		common.Log.WithFields(logrus.Fields{
-			"ip":        ip,
-			"cache-key": hash,
-		}).Info("Filter request with ip")
-		if err := common.Cache.Set(cacheKey, "", 2*time.Second).Err(); err != nil {
-			common.Log.Error(err)
+			if err != redis.Nil {
+				return controllers.Status(c, codes.RepeatRequestWithIp, err)
+			}
+			common.Log.WithFields(logrus.Fields{
+				"ip":        ip,
+				"cache-key": hash,
+			}).Info("Filter request with ip")
+			if err := common.Cache.Set(cacheKey, "", 2*time.Second).Err(); err != nil {
+				common.Log.Error(err)
+			}
 		}
 		if err := next(c); err != nil {
 			c.Error(err)
@@ -52,42 +54,49 @@ func FilterRequestsStrict(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		hash := c.Request().Header.Get("X-Request-Hash")
 		cacheKey := hashString(config.Config.Redis.CachePrefix, "strict-filter", hash)
-		_, err := common.Cache.Get(cacheKey).Result()
+		if c.Request().Method == echo.POST || c.Request().Method == echo.PUT {
+			_, err := common.Cache.Get(cacheKey).Result()
 
-		if err != nil && err != redis.Nil {
-			return controllers.Status(c, codes.CacheError, err)
-		}
+			if err != nil && err != redis.Nil {
+				return controllers.Status(c, codes.CacheError, err)
+			}
 
-		if err != redis.Nil {
-			return controllers.Status(c, codes.RepeatRequestStrict, err)
+			if err != redis.Nil {
+				return controllers.Status(c, codes.RepeatRequestStrict, err)
+			}
+			common.Cache.Set(cacheKey, "", 60*time.Second)
 		}
-		common.Cache.Set(cacheKey, "", 60*time.Second)
 		if err := next(c); err != nil {
 			c.Error(err)
 		}
-		common.Cache.Del(cacheKey)
+		if c.Request().Method == echo.POST || c.Request().Method == echo.PUT {
+			common.Cache.Del(cacheKey)
+		}
 		return nil
 	}
 }
 
 func FilterIteratedRequests(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		hash := c.Request().Header.Get("X-Request-Hash")
-		token := c.Request().Header.Get("X-Access-Token")
-		ip := c.RealIP()
-		cacheKey := hashString(config.Config.Redis.CachePrefix, "token-filter", ip, token, hash)
-		_, err := common.Cache.Get(cacheKey).Result()
+		if c.Request().Method == echo.POST || c.Request().Method == echo.PUT {
 
-		if err != nil && err != redis.Nil {
-			return controllers.Status(c, codes.CacheError, err)
-		}
+			hash := c.Request().Header.Get("X-Request-Hash")
+			token := c.Request().Header.Get("X-Access-Token")
+			ip := c.RealIP()
+			cacheKey := hashString(config.Config.Redis.CachePrefix, "token-filter", ip, token, hash)
+			_, err := common.Cache.Get(cacheKey).Result()
 
-		if err != redis.Nil {
-			return controllers.Status(c, codes.RepeatRequestWithToken, err)
-		}
-		common.Cache.Set(cacheKey, "", 2*time.Second)
-		if err := next(c); err != nil {
-			c.Error(err)
+			if err != nil && err != redis.Nil {
+				return controllers.Status(c, codes.CacheError, err)
+			}
+
+			if err != redis.Nil {
+				return controllers.Status(c, codes.RepeatRequestWithToken, err)
+			}
+			common.Cache.Set(cacheKey, "", 2*time.Second)
+			if err := next(c); err != nil {
+				c.Error(err)
+			}
 		}
 		// common.Cache.Del(cacheKey)
 		return nil
