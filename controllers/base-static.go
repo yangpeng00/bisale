365618dad23328ffa65c88832aa4ccafb53ce3e3
koyeo <im@koyeo.io>
2018-08-13 15:52:18 +0800

    debug

--- controllers/base-static.go
+++ controllers/base-static.go
@@ -23,11 +23,11 @@ func GetStatic(c echo.Context) error {
 	userKycService, userKycClient := common.GetBisaleUserKycServiceClient()
 	defer common.BisaleUserKycServicePool.Put(userKycClient)
 
-	userCount, err := userService.SelectSlaveAllUserCount(context.Background(), traceId)
-	if err != nil {
-		log.Error(err)
-		return Status(c, codes.ServiceError, err)
-	}
+	//userCount, err := userService.SelectSlaveAllUserCount(context.Background(), traceId)
+	//if err != nil {
+	//	log.Error(err)
+	//	return Status(c, codes.ServiceError, err)
+	//}
 	kycCount, err := userKycService.SelectSlaveAllUserKycCount(context.Background(), traceId)
 	if err != nil {
 		log.Error(err)
@@ -44,7 +44,7 @@ func GetStatic(c echo.Context) error {
 		return Status(c, codes.ServiceError, err)
 	}
 	staticResponse := new(StaticResponse)
-	staticResponse.UserCount = userCount
+	//staticResponse.UserCount = userCount
 	staticResponse.KycCount = kycCount
 	staticResponse.RegisterDailyCount = registerCountDay
 	staticResponse.UserKycDailyCount = kycCountDay
